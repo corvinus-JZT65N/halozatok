a3b33538408db79cb369961d372e9a30da2edcea
Kiss Gergely <gergely.kiss3@stud.uni-corvinus.hu>
2021-03-18 12:28:13 +0100

    JSgyak: Valami miatt ha .js-t hivatkozok be, akkor töröl minden mást a HTML oldalon. Ha viszont így, scriptként, akkor bár felismeri, hogy ez egy script, nem csinál semmit. Maga a feladat működik, táblázatokban meg is tudnám csinálni, íhy azonban nem hozza az elvárt működést.

---
+++ HajosTeszut/HajosTeszut/wwwroot/jsgyak.js
@@ -0,0 +1,27 @@
+﻿window.onload = function () {
+console.log("Oldal betöltve...");
+    console.log("minta fut!")
+
+
+    var faktoriális = function (n) {
+        let eredmeny = 1;
+
+        for (var i = 2; i <= n; i++) {
+            eredmeny = eredmeny * i;
+        }
+        return eredmeny;
+    }
+
+document.wriln("<div id='pascal'>");
+    console.log("betöltődött")
+for (var i = 0; i < 10; i++) {
+    document.write("<div class='sor'>");
+    for (var j = 0; j <= i; j++) {
+        var eredmeny = 1;
+        eredmeny = faktoriális(i) / (faktoriális(j) * (faktoriális(i - j)));
+        document.write("<div class='elem'>"+eredmeny+"</div>")
+    }
+    document.write('</div>');
+    }
+    document.write('</div>');
+}
