6ec193e7dab66218038f9a3d2fb9eeb90dba2a3a
Gergely Kiss <gergely.kiss3@stud.uni-corvinus.hu>
2021-04-28 17:50:42 +0200

    Gyak9

--- HajosTeszut/HajosTeszut/wwwroot/hetedik.js
+++ HajosTeszut/HajosTeszut/wwwroot/hetedik.js
@@ -17,14 +17,28 @@ var helyesValasz;
         kérdésMegjelenítés(kérdésSorszám);
     }
 
-/*function kérdésMegjelenítés2(kérdés) {
+function kérdésMegjelenítés2(kérdés) {
     console.log(kérdés);
-    kérdés_szöveg.innerText = kérdés.questionText
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
     document.getElementById("válasz1").innerText = kérdés.answer1
     document.getElementById("válasz2").innerText = kérdés.answer2
     document.getElementById("válasz3").innerText = kérdés.answer3
     document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
-}*/
+    kérdésBetöltés(kérdésSorszám);
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
 
 var kérdésMegjelenítés = function (kérdésSzáma) {
     let kérdés_szöveg = document.getElementById("kérdés_szöveg");
